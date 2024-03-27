cea31d30292495bbccb0308aafcc77e3afb5c078
RUAD <RUAD@RUADZEHRA>
2024-03-27 22:58:09 +0300

    Добавьте файлы проекта.

---
+++ TicTacToeClient/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TicTacToeClient
+{
+    public partial class MainWindow : Window
+    {
+        private static readonly Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private const int port = 27001;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectToServer();
+            RequestLoop();
+        }
+
+        private void RequestLoop()
+        {
+            var receiver = Task.Run(() =>
+            {
+                while (true)
+                {
+                    ReceiveResponse();
+                }
+            });
+        }
+
+        private void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+
+            IntegrateToView(text);
+
+        }
+
+        private void IntegrateToView(string text)
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                var data = text.Split('\n');
+                var row1 = data[0].Split('\t');
+                var row2 = data[1].Split('\t');
+                var row3 = data[2].Split('\t');
+
+                b1.Content = row1[0];
+                b2.Content = row1[1];
+                b3.Content = row1[2];
+
+                b4.Content = row2[0];
+                b5.Content = row2[1];
+                b6.Content = row2[2];
+
+                b7.Content = row3[0];
+                b8.Content = row3[1];
+                b9.Content = row3[2];
+            });
+        }
+
+        private void ConnectToServer()
+        {
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    ClientSocket.Connect(IPAddress.Parse("192.168.1.69"), port);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            MessageBox.Show("Player X or O: Connected to game");
+
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+
+            string text = Encoding.ASCII.GetString(data);
+
+            this.Title = "Player : " + text;
+            this.player.Text = this.Title;
+
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    var bt = sender as Button;
+                    string request = bt.Content.ToString() + player.Text.Split(' ')[2];
+                    SendString(request);
+                });
+            });
+        }
+
+        private void SendString(string request)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(request);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+    }
+}
